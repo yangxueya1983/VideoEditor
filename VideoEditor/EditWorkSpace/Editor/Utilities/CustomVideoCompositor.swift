fd5d369eb2d9e4ff8fc1dfb3df2d957c66a71ca6
yy <02dzxxyy@163.com>
2024-08-18 21:42:41 -0700

    refactor custom composition instruction

--- VideoEditor/EditWorkSpace/Editor/Utilities/CustomVideoCompositor.swift
+++ VideoEditor/EditWorkSpace/Editor/Utilities/CustomVideoCompositor.swift
@@ -10,6 +10,52 @@ import AVFoundation
 import CoreVideo
 import CoreImage
 
+class CustomVideoCompositionInstructionBase : AVMutableVideoCompositionInstruction {
+    func compose(_ frontSample: CIImage, _ backgroundSample: CIImage, _ process : CGFloat, _ size: CGSize) -> CIImage? {
+        return nil
+    }
+}
+
+class CrossDissolveCompositionInstruction : CustomVideoCompositionInstructionBase {
+    override func compose(_ frontSample: CIImage, _ backgroundSample: CIImage, _ process: CGFloat, _ size: CGSize) -> CIImage? {
+        let blendedImage = frontSample.applyingFilter("CIBlendWithAlphaMask", parameters: [
+            kCIInputBackgroundImageKey: backgroundSample,
+            kCIInputMaskImageKey: CIImage(color: CIColor(red: 1, green: 1, blue: 1, alpha: process)).cropped(to: frontSample.extent)
+        ])
+        
+        return blendedImage
+    }
+}
+
+class CircleEnlargerCompositionInstruction : CustomVideoCompositionInstructionBase {
+    func createCenterRadiusMask(size: CGSize, progress: CGFloat) -> CIImage? {
+        let center = CIVector(x: size.width / 2, y: size.height / 2)
+        let radius = sqrt(size.width * size.width +  size.height * size.height) / 2  * progress
+        
+        // Create a radial gradient filter for the transition effect
+        let gradientFilter = CIFilter(name: "CIRadialGradient")!
+        gradientFilter.setValue(center, forKey: "inputCenter")
+        gradientFilter.setValue(radius, forKey: "inputRadius0") // Inner radius (start of gradient)
+        gradientFilter.setValue(radius + 1, forKey: "inputRadius1") // Outer radius (end of gradient)
+        gradientFilter.setValue(CIColor.white, forKey: "inputColor0") // Inside color (visible area)
+        gradientFilter.setValue(CIColor.black, forKey: "inputColor1") // Outside color (masked area)
+        
+        // Crop the gradient to the image size
+        return gradientFilter.outputImage?.cropped(to: CGRect(origin: .zero, size: size))
+    }
+    
+    override func compose(_ frontSample: CIImage, _ backgroundSample: CIImage, _ process: CGFloat, _ size: CGSize) -> CIImage? {
+        let maskImage = self.createCenterRadiusMask(size: size, progress: process)
+        let blendFilter = CIFilter(name: "CIBlendWithMask")
+        blendFilter?.setValue(frontSample, forKey: kCIInputImageKey)
+        blendFilter?.setValue(backgroundSample, forKey: kCIInputBackgroundImageKey)
+        blendFilter?.setValue(maskImage, forKey: kCIInputMaskImageKey)
+        
+        return blendFilter?.outputImage
+    }
+}
+
+
 
 class CustomVideoCompositor: NSObject, AVVideoCompositing {
     private let renderContextQueue = DispatchQueue(label: "com.example.CustomVideoCompositor.renderContextQueue")
@@ -44,6 +90,8 @@ class CustomVideoCompositor: NSObject, AVVideoCompositing {
                 return
             }
             
+            let videoSize = renderContext.size
+            
             // Retrieve source frames
             guard let foregroundFrame = asyncVideoCompositionRequest.sourceFrame(byTrackID: asyncVideoCompositionRequest.sourceTrackIDs[0].int32Value),
                   let backgroundFrame = asyncVideoCompositionRequest.sourceFrame(byTrackID: asyncVideoCompositionRequest.sourceTrackIDs[1].int32Value) else {
@@ -53,22 +101,22 @@ class CustomVideoCompositor: NSObject, AVVideoCompositing {
             
             // Apply transition effect (crossfade example)
             let transitionFactor =  CGFloat(CMTimeGetSeconds(asyncVideoCompositionRequest.compositionTime) / CMTimeGetSeconds(asyncVideoCompositionRequest.videoCompositionInstruction.timeRange.duration))
-            print("transition factor is \(transitionFactor)")
             let outputPixelBuffer = renderContext.newPixelBuffer()
             
             // Create CIImages from the pixel buffers
             let ciForeground = CIImage(cvPixelBuffer: foregroundFrame)
             let ciBackground = CIImage(cvPixelBuffer: backgroundFrame)
             
-            // Blend the images using a crossfade
-            let blendedImage = ciForeground.applyingFilter("CIBlendWithAlphaMask", parameters: [
-                kCIInputBackgroundImageKey: ciBackground,
-                kCIInputMaskImageKey: CIImage(color: CIColor(red: 1, green: 1, blue: 1, alpha: transitionFactor)).cropped(to: ciForeground.extent)
-            ])
+            guard let instruction = asyncVideoCompositionRequest.videoCompositionInstruction as? CustomVideoCompositionInstructionBase else {
+                asyncVideoCompositionRequest.finish(with: NSError(domain: "instruction is not CustomVideoCompositionInstructionBase", code: 0))
+                return
+            }
+            
+            let blendedImage = instruction.compose(ciForeground, ciBackground, transitionFactor, videoSize)
             
             // Render the blended image into the output buffer
             let ciContext = CIContext()
-            ciContext.render(blendedImage, to: outputPixelBuffer!)
+            ciContext.render(blendedImage!, to: outputPixelBuffer!)
             
             // Finish the request with the output pixel buffer
             asyncVideoCompositionRequest.finish(withComposedVideoFrame: outputPixelBuffer!)
